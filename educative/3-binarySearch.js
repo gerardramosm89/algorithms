5b74ae3903db88971af3d2490abfbcccef5a3ec1
Gerard Ramos <gerard.ramosm89@gmail.com>
2017-09-30 11:55:16 -0700

    binarysearch

--- educative/3-binarySearch.js
+++ educative/3-binarySearch.js
@@ -1,1 +1,17 @@
 var arr = [1,10,20,47,59,63,75,88,99,107];
+// The point of the binary search will be to keep cutting the array in half until we find  our 
+function binarySearch(arr, min, max, val) {
+  if (min > max) return -1;
+  let mid = min + Math.floor(max - min / 2);
+  if (val > arr[mid]) {
+    min = mid + 1;
+    return binarySearch(arr, min, max, val);
+  } else if (val < arr[mid]) {
+    max = mid - 1;
+    return binarySearch(arr, min, max, val);
+  } else if (val === arr[mid]) {
+    return arr[mid];
+  }
+}
+
+console.log(binarySearch(arr, 0, arr.length - 1, 47));
