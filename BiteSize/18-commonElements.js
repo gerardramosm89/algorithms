c3b016b220452d006a5e80daef723e5bde461a37
Gerard Ramos <gerard.ramosm89@gmail.com>
2017-10-17 21:25:07 -0700

    updating common elements

--- BiteSize/18-commonElements.js
+++ BiteSize/18-commonElements.js
@@ -7,15 +7,23 @@
   result = [1,4,9];
 */
 
-const a = [1,3,4,6,7,9];
-const b = [1,2,4,5,9,10];
+const a = [1,3,4,6,7,9,11,22];
+const b = [1,2,4,5,9,10,22];
 
 function solution(a, b) {
   let aIndex = 0;
   let bIndex = 0;
-  a.length < b.length ? shortest = a.length : shortest = b.length;
+  let shortestArray;
+  function setShortest(x) {
+    if (x == 'a') {
+      shortest = a.length;
+    } else {
+      shortest = b.length;
+    }
+  }
+  a.length < b.length ? setShortest('a') : setShortest('b');
   let results = [];
-  while (aIndex < shortest) {
+  while ((aIndex < a.length) && (bIndex < b.length)) {
     if (a[aIndex] === b[bIndex]) {
       results.push(a[aIndex]);
       aIndex++;
