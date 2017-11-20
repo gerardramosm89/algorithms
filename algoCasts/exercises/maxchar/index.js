921891de1d7d1c01d60de15843a52ee5a3087f4c
gerardramosayasdi <gerard.ramosm89@gmail.com>
2017-11-19 19:38:04 -0800

    adding maxchars

--- algoCasts/exercises/maxchar/index.js
+++ algoCasts/exercises/maxchar/index.js
@@ -5,6 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
-
+function maxChar(str) {
+  let list = {};
+  let highestVal = 0;
+  str.split('').map(char => {
+    if(list[char]) list[char] += 1;
+    else(list[char] = 1);
+    if (list[char] > highestVal) {
+      highestVal = list[char];
+      currentHighest = char;
+    };
+  });
+  console.log('list is: ', list);
+  console.log('current highest is: ', currentHighest);
+  return currentHighest;
+}
+maxChar('Hello There!');
 module.exports = maxChar;
