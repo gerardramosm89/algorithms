ffc8dad726ff9477618a3feaa8e57bd4df75c4fe
gerardramosayasdi <gerard.ramosm89@gmail.com>
2017-11-23 20:02:40 -0800

    adding chunks

--- algoCasts/exercises/chunk/index.js
+++ algoCasts/exercises/chunk/index.js
@@ -8,6 +8,30 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+const log = (x, y) => console.log(x, y);
 
+function chunk(array, size) {
+  const chunked = [];
+  for (let el of array) {
+    console.log('el we are looking at is: ', el);
+    const last = chunked[chunked.length - 1];
+    log('last: ', last);    
+    if (!last || last.length === size) {
+      chunked.push([el]);
+    } else {
+      last.push(el);
+    }
+  }
+  console.log('chunked: ', chunked);
+  return chunked;
+}
+
+// function chunk(array, size) {
+//   const result = [];
+//   for (let el of array) {
+//     console.log('el: ', el);
+//   }
+//   console.log('result: ', result);
+// }
+chunk([44,324,543,144,545], 2);
 module.exports = chunk;
