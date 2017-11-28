9b13fb5c87319285f70d15fa09714811e13ce599
gerardramosayasdi <gerard.ramosm89@gmail.com>
2017-11-27 21:33:37 -0800

    adding steps

--- algoCasts/exercises/steps/index.js
+++ algoCasts/exercises/steps/index.js
@@ -18,7 +18,17 @@
 //       '####'
 
 function steps(n) {
-  
+  for (let row = 0; row < n; row++) {
+    let stair = '';
+    for (let col = 0; col < n; col++) {
+      if (col <= row) {
+        stair += '#';
+      } else {
+        stair += ' ';
+      }
+    }
+    console.log(stair);  
+  }
 }
-
+steps(4);
 module.exports = steps;
