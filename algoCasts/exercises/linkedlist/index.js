4a0818ddfc649ca0e2ed64720660087c86f51280
gerardramosayasdi <gerard.ramosm89@gmail.com>
2018-02-18 11:31:17 -0800

    adding inserfirst ll

--- algoCasts/exercises/linkedlist/index.js
+++ algoCasts/exercises/linkedlist/index.js
@@ -2,8 +2,29 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+
+  insertFirst(data) {
+    let node = new Node(data);
+    if (this.head) {
+      node.next = this.head;
+      node.data = data;
+      this.head = node;
+    } else {
+      this.head = node;
+    }
+  }
+
+}
 
 module.exports = { Node, LinkedList };
